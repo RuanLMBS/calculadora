c2b7c500508efee5227aa17bc1a712ceaf42556b
RuanLMBS <ulle-2003@hotmail.com>
2025-07-18 17:22:51 -0300

    Adicionando interface no console para usuário

--- src/ui/ConsoleInterface.java
+++ src/ui/ConsoleInterface.java
@@ -1,7 +1,31 @@
 package ui;
 
+import core.Calculadora;
+import utils.InputHelper;
+
 public class ConsoleInterface {
     public static void Iniciar() {
+        System.out.println("=== Calculadora ===");
 
+        boolean continuar;
+        do{
+            double resultado = InputHelper.lerNumero("Digite o primeiro número: ");
+            while(true) {
+                String operador = InputHelper.lerOperador("Digite um operador (+, -, *, /) ou '=' para finalizar: ");
+                if(operador.equals("=")) {
+                    break;
+                }
+                double proximoValor = InputHelper.lerNumero("Digite o próximo número: ");
+                resultado = Calculadora.calcular(resultado, proximoValor, operador);
+                System.out.println("Resultado parcial: "+resultado);
+
+            }
+
+            System.out.println("Resultado Final: "+resultado);
+            continuar = InputHelper.confirmar("Deseja realizar outra operação? (S/N): ");
+
+        } while(continuar);
+
+        System.out.println("Obrigado por utilizar a calculadora!");
     }
 }

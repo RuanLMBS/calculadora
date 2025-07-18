81d4064c51eb74b712d452d4bf668bf97a84a77b
RuanLMBS <ulle-2003@hotmail.com>
2025-07-18 17:07:42 -0300

    Adicionando funcionalidades de leitura de inputs

---
+++ src/utils/InputHelper.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class InputHelper {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static double lerNumero(String msg) {
+        while (true) {
+            try {
+                System.out.println(msg);
+                return Double.parseDouble(sc.nextLine().replace(",","."));
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: " + e);
+            }
+        }
+    }
+
+    public static String lerOperador (String operador) {
+        while(true) {
+            System.out.println(operador);
+            String input = sc.nextLine().trim();
+            if (input.matches("[+\\-*/=]")) {
+                return input;
+            } else {
+                System.out.println("Operador inválido!");
+            }
+
+        }
+    }
+
+    public static boolean confirmar(String msg) {
+        System.out.println(msg);
+        String resposta = sc.nextLine().trim().toUpperCase(Locale.ROOT);
+        return resposta.equals("S");
+    }
+}
